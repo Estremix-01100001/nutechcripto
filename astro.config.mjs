3e3f311355cfd2b516163d1c78973cb3e7e31c55
Ant <anthonyreyes12.06.25.dignitatec@gmail.com>
2025-09-12 02:59:19 -0500

    feat: Configurar base para GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://estremix-01100001.github.io',
   base: '/nutechcripto', // <-- Añade esta línea
+  integrations: [tailwind()]
 });
