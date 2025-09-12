f7d187f3102d85e0d90fa6da8376af63c351e98e
Ant <anthonyreyes12.06.25.dignitatec@gmail.com>
2025-09-12 02:21:51 -0500

    fix: Correct base path for Tailwind styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://estremix-01100001.github.io/nutechcritto/",
+  site: "https://estremix-01100001.github.io/nutechcripto/",
   base: "/nutechcripto/",
 });
