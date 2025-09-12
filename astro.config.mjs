3f9a402de1a2663a58b7bb1723e340bdaf9e6f97
Ant <anthonyreyes12.06.25.dignitatec@gmail.com>
2025-09-12 02:45:19 -0500

    feat: Configurar base de Astro para GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://estremix-01100001.github.io/nutechcripto/",
-  base: "/nutechcripto/",
+  site: 'https://estremix-01100001.github.io',
+  base: '/nutechcripto', // <-- Añade esta línea
 });
